1dbdfd62b3d9b6026c69e3e4917a62a278c5054d
edgar <alexdta@gmail.com>
2020-09-14 12:29:00 -0600

    Primer Commit

---
+++ send_mail/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace send_mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrigen = new System.Windows.Forms.TextBox();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPuerto = new System.Windows.Forms.NumericUpDown();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAdjunto = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnAdjunto = new System.Windows.Forms.Button();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.chkHtml = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPuerto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(127, 586);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Correo Origen";
+            // 
+            // txtOrigen
+            // 
+            this.txtOrigen.Location = new System.Drawing.Point(127, 20);
+            this.txtOrigen.Name = "txtOrigen";
+            this.txtOrigen.Size = new System.Drawing.Size(306, 22);
+            this.txtOrigen.TabIndex = 2;
+            this.txtOrigen.Text = "<user>@gmail.com";
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(127, 184);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(306, 22);
+            this.txtDestino.TabIndex = 4;
+            this.txtDestino.Text = "<destino>@gmail.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Correo Destino";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(127, 100);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(306, 22);
+            this.txtServidor.TabIndex = 8;
+            this.txtServidor.Text = "smtp.gmail.com";
+            this.toolTip.SetToolTip(this.txtServidor, "cambiar según el servidor de correo");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Servidor SMTP";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(127, 59);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(306, 22);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.Text = "<password>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contraseña";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Puerto";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(127, 140);
+            this.txtPuerto.Maximum = new decimal(new int[] {
+            2525,
+            0,
+            0,
+            0});
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(120, 22);
+            this.txtPuerto.TabIndex = 10;
+            this.toolTip.SetToolTip(this.txtPuerto, "Posibles Puertos\r\n25\r\n587\r\n2525");
+            this.txtPuerto.Value = new decimal(new int[] {
+            587,
+            0,
+            0,
+            0});
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Location = new System.Drawing.Point(127, 228);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(306, 22);
+            this.txtAsunto.TabIndex = 12;
+            this.txtAsunto.Text = "Correo de Prueba";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Asunto";
+            // 
+            // txtAdjunto
+            // 
+            this.txtAdjunto.Enabled = false;
+            this.txtAdjunto.Location = new System.Drawing.Point(127, 274);
+            this.txtAdjunto.Name = "txtAdjunto";
+            this.txtAdjunto.Size = new System.Drawing.Size(306, 22);
+            this.txtAdjunto.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 277);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Archivo Adjunto";
+            // 
+            // btnAdjunto
+            // 
+            this.btnAdjunto.Location = new System.Drawing.Point(439, 273);
+            this.btnAdjunto.Name = "btnAdjunto";
+            this.btnAdjunto.Size = new System.Drawing.Size(75, 23);
+            this.btnAdjunto.TabIndex = 15;
+            this.btnAdjunto.Text = "Archivo";
+            this.btnAdjunto.UseVisualStyleBackColor = true;
+            this.btnAdjunto.Click += new System.EventHandler(this.btnAdjunto_Click);
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(127, 318);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(306, 199);
+            this.txtMensaje.TabIndex = 17;
+            this.txtMensaje.Text = resources.GetString("txtMensaje.Text");
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 321);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Mensaje";
+            // 
+            // chkHtml
+            // 
+            this.chkHtml.AutoSize = true;
+            this.chkHtml.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chkHtml.Checked = true;
+            this.chkHtml.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkHtml.Location = new System.Drawing.Point(127, 536);
+            this.chkHtml.Name = "chkHtml";
+            this.chkHtml.Size = new System.Drawing.Size(183, 21);
+            this.chkHtml.TabIndex = 18;
+            this.chkHtml.Text = "Mensaje en HTML Si/No";
+            this.chkHtml.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chkHtml.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 632);
+            this.Controls.Add(this.chkHtml);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnAdjunto);
+            this.Controls.Add(this.txtAdjunto);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOrigen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnviar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enviar Correo";
+            ((System.ComponentModel.ISupportInitialize)(this.txtPuerto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrigen;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown txtPuerto;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAdjunto;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnAdjunto;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox chkHtml;
+    }
+}
+
---
+++ send_mail/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace send_mail
+{
+    public partial class Form1 : Form
+    {
+
+        #region Constructor
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Metodos
+
+        private void enviarMail(
+                                string pCorreoOrigen,
+                                string pContrasenaOrigen,
+                                string pServSMTP,
+                                int pPuertoSMPT,
+                                string pCorreoDestino,
+                                string pAsunto,
+                                string pAdjunto,
+                                string pMensaje,
+                                bool pHtml)
+        {
+            try
+            {
+
+                MailMessage lMensaje = new MailMessage(pCorreoOrigen, pCorreoDestino)
+                {
+                    Subject = pAsunto,
+                    IsBodyHtml = pHtml,
+                    Body = pMensaje,
+                };
+                if (pAdjunto != null)
+                {
+                    lMensaje.Attachments.Add(new Attachment(pAdjunto));
+                }
+
+                SmtpClient lCorreo = new SmtpClient()
+                {
+                    Port = pPuertoSMPT,
+                    Host = pServSMTP,
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential(pCorreoOrigen, pContrasenaOrigen)
+                };
+
+                lCorreo.Send(lMensaje);
+
+                MessageBox.Show("Listo");
+            }
+            catch (Exception lExcp)
+            {
+                MessageBox.Show(lExcp.Message);
+            }
+        }
+
+        public static bool verificarConexion()
+        {
+            Uri Url = new System.Uri("http://www.microsoft.com");
+
+            WebRequest WebReq;
+            WebResponse Resp;
+            WebReq = WebRequest.Create(Url);
+
+            try
+            {
+                Resp = WebReq.GetResponse();
+                Resp.Close();
+                WebReq = null;
+                return true;
+            }
+            catch
+            {
+                WebReq = null;
+                return false;
+            }
+        }
+
+        public static bool verificarConexion2()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Botones
+
+        private void btnAdjunto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog lArchivo = new OpenFileDialog()
+            {
+                InitialDirectory = Environment.SpecialFolder.Desktop.ToString(),
+                Multiselect = false
+            };
+
+            DialogResult lResult = lArchivo.ShowDialog();
+
+            if (lResult != DialogResult.Cancel)
+            {
+                txtAdjunto.Text = lArchivo.FileName;
+            }
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (verificarConexion2())
+            {
+                string lCorreoOrigen = txtOrigen.Text.Trim();
+                string lContrasenaOrigen = txtPassword.Text.Trim();
+                string lServSMTP = txtServidor.Text.Trim();
+                int lPuertoSMPT = Convert.ToInt32(txtPuerto.Value);
+                string lCorreoDestino = txtDestino.Text.Trim();
+                string lAsunto = txtAsunto.Text.Trim();
+                string lAdjunto = txtAdjunto.Text.Length > 0 ? txtAdjunto.Text : null;
+                string lMensaje = txtMensaje.Text;
+                bool lHtml = chkHtml.Checked;
+
+                enviarMail(
+                    lCorreoOrigen,
+                    lContrasenaOrigen,
+                    lServSMTP,
+                    lPuertoSMPT,
+                    lCorreoDestino,
+                    lAsunto,
+                    lAdjunto,
+                    lMensaje,
+                    lHtml);
+            }
+            else
+            {
+                MessageBox.Show("No hay conexión a internet\nNo se puede enviar el correo", "Atención");
+            }
+
+
+        }
+
+        #endregion
+
+    }
+}
